4d19ab1531ec0a1f1c317931737aed69f97dcb8a
Dark <dillon.dommer@gmail.com>
2017-11-20 19:27:15 -0500

    update docstrings

--- cipher.py
+++ cipher.py
@@ -7,13 +7,13 @@ def generateCipherSuite():
     return cipherSuite
 
 def encrypt(plainText, cipherSuite):
-    '''Takes a string and returns the corresponding encrypted bytes literal'''
+    '''Takes a string, Fernet object and returns the corresponding encrypted bytes literal'''
     encodedPass = plainText.encode('UTF-8') # convert password (string) to bytes literal
     cipherText = cipherSuite.encrypt(encodedPass) # encrypt
     return cipherText
     
 def decrypt(cipherText, cipherSuite):
-    '''Takes a bytes literal and returns the corresponding decrypted string'''
+    '''Takes a bytes literal, Fernet object and returns the corresponding decrypted string'''
     encodedPlainText = cipherSuite.decrypt(cipherText) # decrypt
     plainText = encodedPlainText.decode('UTF-8') # convert decrypted password to string
     return plainText
