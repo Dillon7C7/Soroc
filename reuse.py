da917f3c93e2f725f5f00dde84d06a378f8c2936
Dark <dillon.dommer@gmail.com>
2017-11-21 20:22:07 -0500

    change excel dictionary value

--- reuse.py
+++ reuse.py
@@ -24,20 +24,28 @@ for a in range(1, len(sys.argv)):
     sysArgs[sysArgNames[a]] = sys.argv[a]
 
 
-# {serial: {asset: (model, comment)}}
+# {serial: (asset, model, comment)}
 HDD = {}
 
 print('Opening excel...')
-workBook = openpyxl.load_workbook('HDDs.xlsx')
+try:
+    workBook = openpyxl.load_workbook('HDDs.xlsx')
+except FileNotFoundError:
+    print('daaayyyym')
+    sys.exit()
 sheets = workBook.get_sheet_names()
 HDDsheet = workBook.get_sheet_by_name(sheets[0])
 
+
+def excelData(excelSheet, element):
+    return str(excelSheet[sysArgs[element].upper + str(row)].value).upper()
+
 for row in range(int(sysArgs['startRow']), int(sysArgs['endRow']) + 1): # iterate through first row to last
 
-    serial = str(HDDsheet[sysArgs['serialCol'].upper() + str(row)].value).upper()
-    asset = str(HDDsheet[sysArgs['assetCol'].upper() + str(row)].value).upper()
-    model = str(HDDsheet[sysArgs['modelCol'].upper() + str(row)].value).upper()
-    comment = str(HDDsheet[sysArgs['commentCol'].upper() + str(row)].value).upper()
+    serial = excelData(HDDsheet, 'serialCol')
+    asset = excelData(HDDsheet, 'assetCol')
+    model = excelData(HDDsheet, 'modelCol')
+    comment = excelData(HDDsheet, 'commentCol')
 
     if serial == 'NONE': # skip the row if there are no values
         continue
@@ -47,28 +55,11 @@ for row in range(int(sysArgs['startRow']), int(sysArgs['endRow']) + 1): # iterat
     HDD[serial] = (asset, model, comment)
 
 
-    # HDD is a dictionary whose key is a serial and value is a dictionary
-    # the inner dictionary's key is an asset tag and value is a
-    # tuple of models and comments (perhaps sell/scrap as well in the future)
-    #HDD[serial] = {asset: (model, comment)}
-
-
 ## -------------------------------------------------------------------------
-
-##r = ['serial', 'asset', 'model', 'comment']
-
-##def practice(l, val):
-##    print(l + ' is: ' + val) 
+## remove later...
 for k, v in HDD.items():
     print('serial is: ' + k)
     print('asset is: ' + v[0])
     print('model is: ' + v[1])
     print('comment is: ' + v[2])
     print()
-##    for i in range(len(r)):
-##        if i == 0:
-##            practice(r[i], k)
-##        else:
-##            practice(r[i], v[i-1])
-##    print()
-##    
